c440ae4536b85301cc8a496c816c489b7f5056a9
2439905184 <suchenglong018@126.com>
2020-06-27 15:35:10 +0800

    添加了下载器

---
+++ download.java
@@ -0,0 +1,10 @@
+import java.awt.event.*;
+
+public class download implements ActionListener
+{		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println(e.getActionCommand());
+		}
+}
--- kde.java
+++ kde.java
@@ -8,15 +8,29 @@ import java.awt.event.*;
 public class kde extends Frame
 {	
 	static kde main_layout;
-	//String s1=URLEncoder.encode(s, "utf-8");
 	public static void main(String[] args) throws IOException
 	{	
 	// cmd /k start
 		kde main_layout=new kde();
 		System.out.println("我");
 		main_layout.setVisible(true);
-		main_layout.setSize(600,600);    
+		main_layout.setSize(800,600);
+		main_layout.setTitle("godot游戏平台");
 		main_layout.setBackground(Color.yellow);
+		
+		Button user_btn=new Button("游客用户");
+		
+		Button download_godot=new Button("下载客户端");
+		download_godot.addActionListener(new download());
+		
+		user_btn.setBounds(0,0,200,200);
+		
+		Panel root=new Panel();
+		main_layout.add(root);
+		
+		root.add(download_godot);
+		root.add(user_btn);
+		
 		main_layout.addWindowListener(new WindowAdapter()
 		{
 		public void windowClosing(WindowEvent e)

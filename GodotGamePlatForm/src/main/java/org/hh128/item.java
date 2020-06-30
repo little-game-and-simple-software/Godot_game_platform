98c208de8f720bfff8f9a31fe1c18f3645c09a29
2439905184 <suchenglong018@126.com>
2020-06-30 11:02:34 +0800

    添加游戏类

---
+++ GodotGamePlatForm/src/main/java/org/hh128/item.java
@@ -0,0 +1,19 @@
+//游戏的封面展示类
+/*-------------------
+	---   游戏的封面 ---
+	----                  --
+	------下载按钮 ----
+	-----是否需要积分:-----     
+
+*/
+public class item extends FlowLayout
+{
+	private String game_name;
+	private Image game_cover;
+	
+	public item()
+	{
+		
+	}
+	
+}
